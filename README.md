0b73c33bd859dcba17203416120f0ba1487aa830
jedm12 <98503725+jedm12@users.noreply.github.com>
2023-02-04 01:52:06 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-cv-2135-jmob-new
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-cv-2135-jmob-sc5dzj)
---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { AboutComponent } from './about/about.component';
+import { EducationComponent } from './education/education.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ReferenceComponent } from './reference/reference.component';
+
+@NgModule({
+  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    AboutComponent,
+    ReferenceComponent,
+    EducationComponent,
+    SkillsComponent,
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
